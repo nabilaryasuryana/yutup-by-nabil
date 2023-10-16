9c3bdecdb2bae1c380c3e9e83751180afa8eedba
nabilaryasuryana <nabil4aryas@gmail.com>
2023-10-16 11:14:18 +0700

    second

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -20,6 +20,7 @@ if (isset($_SESSION['username']) === 0 || isset($_SESSION['username']) === null 
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Itim&display=swap">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+        @vite('resources/css/app.css')
   </head>
     </head>
     <body>
