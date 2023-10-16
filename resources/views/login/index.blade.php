1f52c3246c1e6c5c4f5f2100d14a739bb4a4deea
nabilaryasuryana <nabil4aryas@gmail.com>
2023-10-16 15:19:50 +0700

    perbaruan login

---
+++ resources/views/login/index.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]>      <html class="no-js"> <!--<![endif]-->
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Login To Yutup</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
+        <link rel="icon" type="image/x-icon" href="{{ asset('images/logo.png') }}">
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Itim&display=swap">
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.8.1/flowbite.min.css" rel="stylesheet" />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+        @vite(['resources/css/app.css','resources/js/app.js'])
+    </head>
+    <body>
+        <div class="h-2/3 w-2/4 rounded border border-blue-500 focus:shadow-lg grid focus:shadow-blue-400 m-auto">
+            <div class="row"></div>
+        </div>
+        <script src="{{ asset('js/app.js') }}" async defer></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.8.1/flowbite.min.js"></script>
+    </body>
+</html>
